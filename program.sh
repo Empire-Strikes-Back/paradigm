a4c48352668aebcf949df9a0a816ba6fd3739060
paradigm <paradigm@program>
2021-08-01 02:13:50 +0300

    i remove no-history push from program.sh

--- program.sh
+++ program.sh
@@ -8,15 +8,4 @@ main(){
   clojure -M:main
 }
 
-push(){
-  ORIGIN=$(git remote get-url origin)
-  rm -rf .git
-  git init -b main
-  git remote add origin $ORIGIN
-  git config --local include.path ../.gitconfig
-  git add .
-  git commit -m "i am paradigm program"
-  git push -f -u origin main
-}
-
 "$@"
