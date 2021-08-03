ed29566da587c3c442b7c66e4365e0a9fbce0099
paradigm <paradigm@program>
2021-08-03 14:30:53 +0300

    to program.sh i add j-package

--- program.sh
+++ program.sh
@@ -27,4 +27,30 @@ uberjar(){
   mv out/paradigm.standalone.jar out/jpackage-input/
 }
 
+j-package(){
+  OS=${1:?"Need OS type (windows/linux/mac)"}
+
+  echo "Starting compilation..."
+
+  if [ "$OS" == "windows" ]; then
+    J_ARG="--win-menu --win-dir-chooser --win-shortcut"
+          
+  elif [ "$OS" == "linux" ]; then
+      J_ARG="--linux-shortcut"
+  else
+      J_ARG=""
+  fi
+
+  jpackage \
+    --input out/jpackage-input \
+    --dest out \
+    --main-jar paradigm.standalone.jar \
+    --name "paradigm" \
+    --main-class clojure.main \
+    --arguments -m \
+    --arguments paradigm.main \
+    --app-version "1" \
+    $J_ARG
+}
+
 "$@"
