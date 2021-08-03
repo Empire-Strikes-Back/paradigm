73657be4d1b7d0debc3b010b162a0097650ee366
paradigm <paradigm@program>
2021-08-03 14:28:45 +0300

    i use new commit hash of deps-repl - with clj -X

--- program.sh
+++ program.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 repl(){
-  clj -M:repl
+  clj \
+    -X:repl deps-repl.core/process \
+    :main-ns paradigm.main \
+    :port 7788 \
+    :host '"0.0.0.0"' \
+    :repl? true \
+    :nrepl? false
 }
+
 
 main(){
   clojure -M:main
