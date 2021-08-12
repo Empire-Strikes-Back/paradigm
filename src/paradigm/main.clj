d0bdf0b4d02ece662f5729937329fbf718632767
paradigm <paradigm@program>
2021-08-12 20:53:51 +0300

    i remove spec

--- src/paradigm/main.clj
+++ src/paradigm/main.clj
@@ -6,7 +6,6 @@
                                      timeout to-chan  sliding-buffer dropping-buffer
                                      pipeline pipeline-async]]
    [clojure.string]
-   [clojure.spec.alpha :as s]
    [clojure.java.io :as io])
   (:import
    (javax.swing JFrame JLabel JButton SwingConstants JMenuBar JMenu JTextArea)
@@ -14,7 +13,6 @@
    (java.awt.event WindowListener KeyListener KeyEvent)))
 
 (println "clojure.compiler.direct-linking" (System/getProperty "clojure.compiler.direct-linking"))
-(clojure.spec.alpha/check-asserts true)
 (do (set! *warn-on-reflection* true) (set! *unchecked-math* true))
 
 (defonce stateA (atom nil))
